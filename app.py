4223cf96024b5c5cc1d31b12cab2f67c88a2a0db
tania564738 <taniasophyjose2005@gmail.com>
2025-09-03 00:19:32 +0530

    New

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Home route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Another route
+@app.route("/about")
+def about():
+    return "<h2>This is the About Page</h2>"
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
